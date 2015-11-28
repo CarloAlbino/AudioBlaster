a0e227cd5323068b2e7c98d3f9bf903736e840dd
CarloAlbino <carloalbino@gmail.com>
2015-11-27 20:33:29 -0500

    FirstBossChanges

---
+++ AudioBlaster/Assets/_MyAssets/_Scripts/Enemy/BossProjectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossProjectile : MonoBehaviour {
+
+    public float speed;
+    public float timer;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("DestroySelf", timer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+	}
+
+    void DestroySelf()
+    {
+        Destroy(this.gameObject);
+    }
+}
